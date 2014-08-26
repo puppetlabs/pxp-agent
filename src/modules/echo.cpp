3090db4f3c2f8f8eeeefde76beff581d01f0684e
Richard Clamp <richardc@unixbeard.net>
2014-08-26 15:35:05 +0100

    simplify expression of echo schemas

--- src/modules/echo.cpp
+++ src/modules/echo.cpp
@@ -5,11 +5,13 @@ namespace CthunAgent {
 namespace Modules {
 
 Echo::Echo() {
+    valijson::constraints::TypeConstraint json_type_string(valijson::constraints::TypeConstraint::kString);
+
     valijson::Schema input_schema;
-    input_schema.addConstraint(new valijson::constraints::TypeConstraint(valijson::constraints::TypeConstraint::kString));
+    input_schema.addConstraint(json_type_string);
 
     valijson::Schema output_schema;
-    output_schema.addConstraint(new valijson::constraints::TypeConstraint(valijson::constraints::TypeConstraint::kString));
+    output_schema.addConstraint(json_type_string);
 
     actions["echo"] = Action { input_schema, output_schema };
 }
