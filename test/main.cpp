55e8d4c95859b35195df8763e965dcea25435663
parisiale <alessandro@puppetlabs.com>
2015-01-05 14:25:11 +0000

    (CTH-114) fix catch-based tests; remove gmock
    add new file_utils_test.cpp; add ad-hoc main fuction
    for Catch; remove gmock.

--- test/main.cpp
+++ test/main.cpp
@@ -1,13 +1,13 @@
+// Refer to https://github.com/philsquared/Catch/blob/master/docs/own-main.md
+// for providing our own main function to Catch
+#define CATCH_CONFIG_RUNNER
+
 #include "test/test.h"
 
 #include "src/common/log.h"
 
-int main(int argc, char** argv) {
-    // Configure boost log
-    Cthun::Common::Log::configure_logging(
-        Cthun::Common::Log::log_level::fatal, std::cout);
-
-    // Run all tests
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char* const argv[]) {
+    Cthun::Common::Log::configure_logging(Cthun::Common::Log::log_level::fatal,
+                                          std::cout);
+    return Catch::Session().run(argc, argv);
 }
