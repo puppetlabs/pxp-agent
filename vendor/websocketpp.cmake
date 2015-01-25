593748c34e3d8031c008b67d230fa674d380c08a
parisiale <alessandro@puppetlabs.com>
2015-01-25 18:21:50 +0000

    (CTH-140) Update to websocketpp 0.5.0

--- vendor/websocketpp.cmake
+++ vendor/websocketpp.cmake
@@ -4,8 +4,8 @@ INCLUDE(ExternalProject)
 EXTERNALPROJECT_ADD(
     websocketpp
     PREFIX "${PROJECT_BINARY_DIR}"
-    URL "file://${VENDOR_DIRECTORY}/websocketpp-0.4.0.tar.gz"
-    URL_MD5 "21ed2beaaf2fe3f76783e4b3bbaa8206"
+    URL "file://${VENDOR_DIRECTORY}/websocketpp-0.5.0.tar.gz"
+    URL_MD5 "87cc20feea0692fd2dcda04af31fc60f"
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     BUILD_IN_SOURCE 1
