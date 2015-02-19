3d09c1b21b5734c106936e6437951eab3d63a979
parisiale <alessandro@puppetlabs.com>
2015-02-19 16:39:57 +0000

    (maint) Move the identity member to the Agent class
    Moving the logic to retrieve the Cthun Agent identity from endpoint.cpp
    to agent.cpp. Changing the Agent ctor. Other trivial changes.
    
    Enabling assertion in message.cpp.
    
    Using final underscores for the names of private methods in Agent.
    
    Factoring out the logic to retrieve external modules from the Agent
    ctor.
    
    Adding an unit test for the Agent identity.

--- src/agent.h
+++ src/agent.h
@@ -6,6 +6,9 @@
 #include "src/data_container.h"
 #include "src/websocket/endpoint.h"
 
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/path.hpp>
+
 #include <map>
 #include <memory>
 #include <string>
@@ -14,47 +17,69 @@ namespace CthunAgent {
 
 class Agent {
   public:
-    explicit Agent(std::string bin_path);
+    Agent() = delete;
+
+    // Throw a fatal_error in case it fails to open the client
+    // certificate file to load its identity.
+    Agent(std::string bin_path,
+          std::string ca_crt_path,
+          std::string client_crt_path,
+          std::string client_key_path);
+
     ~Agent();
 
     // Daemon entry point.
-    void startAgent(std::string url,
-                    std::string ca_crt_path,
-                    std::string client_crt_path,
-                    std::string client_key_path);
+    void startAgent(std::string server_url);
+
+    // Identity getter.
+    std::string getIdentity() const;
 
   private:
+    // Certificate paths.
+    std::string ca_crt_path_;
+    std::string client_crt_path_;
+    std::string client_key_path_;
+
+    // Cthun agent identity, as in the cert.
+    std::string identity_;
+
+    // Modules
     std::map<std::string, std::shared_ptr<Module>> modules_;
+
+    // Pointer to the ransport layer endpoint instance.
     std::unique_ptr<WebSocket::Endpoint> ws_endpoint_ptr_;
 
+    // Load the external modules contained in the specified directory.
+    void loadExternalModules_(boost::filesystem::path modules_dir_path);
+
     // Log the loaded modules.
-    void listModules();
+    void listModules_() const;
 
     // Set the event callbacks for the connection.
-    void setConnectionCallbacks();
+    void setConnectionCallbacks_();
 
     // Callback for the transport layer triggered when  connection .
     // Send a login message.
-    void sendLogin();
+    void sendLogin_();
 
     // Add the common envelope entries to the specified container.
-    void addCommonEnvelopeEntries(DataContainer& envelope_entries);
+    void addCommonEnvelopeEntries_(DataContainer& envelope_entries);
 
     // Send a response message with specified request ID and output
     // to the receiver endpoint.
-    void sendResponse(std::string receiver_endpoint,
-                      std::string request_id,
-                      DataContainer output,
-                      std::vector<MessageChunk> debug_chunks);
+    void sendResponse_(std::string receiver_endpoint,
+                       std::string request_id,
+                       DataContainer output,
+                       std::vector<MessageChunk> debug_chunks);
 
     // Callback for the transport layer to handle incoming messages.
     // Parse and validate the passed message; reply to the sender
     // with the requested output.
-    void processMessageAndSendResponse(std::string message_txt);
+    void processMessageAndSendResponse_(std::string message_txt);
 
     // Periodically check the connection state; reconnect the agent
     // in case the connection is not open
-    void monitorConnectionState();
+    void monitorConnectionState_();
 };
 
 }  // namespace CthunAgent
