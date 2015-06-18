40e92e72f557c9e855154969f99cef1a3a662ccc
parisiale <alessandro@puppetlabs.com>
2015-06-18 11:16:05 +0100

    (CTH-227) Add new ActionOutcome struct
    Adding new structure to model the outcome of both internal (that produce
    a JSON result directly) and external (that run in a separate process,
    thus producing stdout / stderr streams) actions.

---
+++ lib/inc/cthun-agent/action_outcome.hpp
@@ -0,0 +1,52 @@
+#ifndef SRC_AGENT_ACTION_OUTCOME_HPP_
+#define SRC_AGENT_ACTION_OUTCOME_HPP_
+
+#include <cthun-client/data_container/data_container.hpp>
+
+#include <string>
+
+namespace CthunAgent {
+
+struct ActionOutcome {
+    enum class Type { Internal, External };
+
+    Type type;
+    std::string stderr;
+    std::string stdout;
+    CthunClient::DataContainer results;
+
+    ActionOutcome() {
+    }
+
+    ActionOutcome(std::string& stderr_,
+                  std::string& stdout_,
+                  CthunClient::DataContainer& results_)
+            : type { Type::External },
+              stderr { stderr_ },
+              stdout { stdout_ },
+              results { results_ } {
+    }
+
+    ActionOutcome(std::string& stderr_,
+                  std::string& stdout_,
+                  CthunClient::DataContainer&& results_)
+            : type { Type::External },
+              stderr { stderr_ },
+              stdout { stdout_ },
+              results { results_ } {
+    }
+
+    ActionOutcome(CthunClient::DataContainer& results_)
+            : type { Type::Internal },
+              results { results_ } {
+    }
+
+    ActionOutcome(CthunClient::DataContainer&& results_)
+            : type { Type::Internal },
+              results { results_ } {
+    }
+};
+
+}  // namespace CthunAgent
+
+#endif  // SRC_AGENT_ACTION_OUTCOME_HPP_
