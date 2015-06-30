407a90d8db107c72c7dc3a22e52bd0af39345835
parisiale <alessandro@puppetlabs.com>
2015-06-30 15:09:24 +0100

    (maint) Add log message call to RequestProcessor
    Logging a possible validation failure for requests.

--- lib/src/request_processor.cc
+++ lib/src/request_processor.cc
@@ -239,7 +239,9 @@ void RequestProcessor::validateRequestContent(const ActionRequest& request) {
         // NB: the registred schemas have the same name as the action
         auto& validator = modules_.at(request.module())->input_validator_;
         validator.validate(request.params(), request.action());
-    } catch (CthunClient::validation_error) {
+    } catch (CthunClient::validation_error& e) {
+        LOG_DEBUG("Invalid '%1% %2%' request %3%: %4%",request.module(),
+                  request.action(), request.id(), e.what());
         throw request_validation_error { "invalid input for '" + request.module()
                                          + " " + request.action() + "'" };
     }
