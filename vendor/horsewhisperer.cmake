7518a77f7fe310faf025b6daef8cea702015f8cd
parisiale <alessandro@puppetlabs.com>
2015-07-10 12:11:55 +0100

    (maint) Update to HorseWhisperer 0.10.0

--- vendor/horsewhisperer.cmake
+++ vendor/horsewhisperer.cmake
@@ -4,8 +4,8 @@ include(ExternalProject)
 externalproject_add(
     horsewhisperer
     PREFIX "${PROJECT_BINARY_DIR}"
-    URL "file://${VENDOR_DIRECTORY}/horsewhisperer-0.9.2.zip"
-    URL_MD5 "7b596e9e728b5d2dc8edde5da732b3a5"
+    URL "file://${VENDOR_DIRECTORY}/horsewhisperer-0.10.0.zip"
+    URL_MD5 "bac6d00b0c93d09b6cc33ba0fb0f3b59"
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     BUILD_IN_SOURCE 1
