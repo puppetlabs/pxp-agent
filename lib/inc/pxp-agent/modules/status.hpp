e094f4d2a557931d42e698ceec698d2e831dc6d4
parisiale <alessandro@puppetlabs.com>
2015-10-02 15:54:53 +0100

    (maint) Use tokens for status service

--- lib/inc/pxp-agent/modules/status.hpp
+++ lib/inc/pxp-agent/modules/status.hpp
@@ -8,6 +8,11 @@ namespace Modules {
 
 class Status : public PXPAgent::Module {
   public:
+    static const std::string UNKNOWN;
+    static const std::string SUCCESS;
+    static const std::string FAILURE;
+    static const std::string RUNNING;
+
     Status();
   private:
     ActionOutcome callAction(const ActionRequest& request);
