95c4f0b04a8a376a1e69760e56eaf1700a1cf4e4
james-stocks <james.stocks@puppetlabs.com>
2015-10-20 17:59:48 +0100

    (maint) Remove files for acceptance test setup from git
    The files for setting up hosts to test from git were a left-over from the pxp-module-puppet repo being merged into pxp-agent repo.
    They should be deleted.

--- acceptance/config/git/options_rerun.rb
+++
@@ -1,8 +0,0 @@
-{
-  :type          => 'git',
-  :forge_host    => 'forge-aio01-petest.puppetlabs.com',
-  :load_path     => './lib/',
-  :repo_proxy    => true,
-  :add_el_extras => true,
-  :'puppetserver-confdir' => '/etc/puppetserver/conf.d',
-}
