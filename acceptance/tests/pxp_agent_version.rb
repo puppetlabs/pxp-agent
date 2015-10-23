8beed334c9c1bddd164b1fc9152d9990dcd65cb4
Iristyle <Iristyle@github>
2015-10-23 15:37:30 -0700

    (maint) Acceptance Don't add Ruby to Windows PATH
     - Rework version check to use a different CWD on Windows, so that the
       user PATH does not have to include vendored Ruby.
    
       This approach is a bit safer for any future tests.

--- acceptance/tests/pxp_agent_version.rb
+++ acceptance/tests/pxp_agent_version.rb
@@ -2,11 +2,16 @@ test_name 'C93805 - pxp-agent - Versioning test'
 
 agent1 = agents[0]
 
+def pxp_version_cmd(platform)
+  if platform.start_with?('windows')
+    # simple hack to ensure OpenSSL is found w/out modifying PATH
+    "cd /cygdrive/c/Program\\ Files/Puppet\\ Labs/Puppet/sys/ruby/bin && ../../../pxp-agent/bin/pxp-agent --version"
+  else
+    'cd /opt/puppetlabs/puppet/bin && ./pxp-agent --version'
+  end
+end
+
 step 'cd into pxp-agent bin folder and check the version'
-pxp_agent_folder = ''
-agent1.platform.start_with?('windows') ?
-  pxp_agent_folder = "/cygdrive/c/Program\\ Files/Puppet\\ Labs/Puppet/pxp-agent/bin" :
-  pxp_agent_folder = "/opt/puppetlabs/puppet/bin"
-on(agent1, "cd #{pxp_agent_folder} && ./pxp-agent --version") do |result|
+on(agent1, pxp_version_cmd(agent1.platform)) do |result|
   assert(/[0-9\.]*/ =~ result.stdout, "Version number should be numbers and periods but was \"#{result.stdout.to_s}\"")
 end
