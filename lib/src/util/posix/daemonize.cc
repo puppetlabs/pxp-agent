puppetlabs/pxp-agent

commit e0537e0289514db634dfe458075e9344f9d98f89

(maint) Fixes for cppcheck

lib/src/util/posix/daemonize.cc
@@ -242,9 +242,9 @@ std::unique_ptr<PIDFile> daemonize() {
 
     // Redirect standard files; we always use boost::log anyway
 
-    freopen("/dev/null", "r", stdin);
-    freopen("/dev/null", "w", stdout);
-    freopen("/dev/null", "w", stderr);
+    stdin = freopen("/dev/null", "r", stdin);
+    stdout = freopen("/dev/null", "w", stdout);
+    stderr = freopen("/dev/null", "w", stderr);
 
     LOG_INFO("Daemonization completed; pxp-agent PID=%1%, PID lock file in '%2%'",
              agent_pid, pidfile);

lib/src/request_processor.cc
@@ -84,7 +84,7 @@ class ResultsStorage {
     std::string status_path;
     lth_jc::JsonContainer action_status;
 
-    void initialize(const ActionRequest& request, const std::string results_dir) {
+    void initialize(const ActionRequest& request, const std::string& results_dir) {
         if (!fs::exists(results_dir)) {
             LOG_DEBUG("Creating results directory for '%1% %2%', transaction "
                        "%3%, in '%4%'", request.module(), request.action(),

lib/tests/unit/util/posix/pid_file_test.cc
@@ -31,7 +31,7 @@ const std::string TMP_DIR { SPOOL_DIR + "/tmp_pid" };
 const std::string PID_FILE_NAME { "pxp-agent.pid" };
 const std::string PID_FILE { PID_DIR + "/" + PID_FILE_NAME };
 
-void initializeTmpPIDFile(const std::string dir, const std::string& txt) {
+void initializeTmpPIDFile(const std::string& dir, const std::string& txt) {
     if (!fs::exists(dir) && !fs::create_directories(dir)) {
         FAIL("failed to create tmp_pid directory");
     }

commit d0b551bc3e488bd224f6c352ed176aaaadd096f3

(maint) Fix compiling on Solaris
Commit e0537e0289514db634dfe458075e9344f9d98f89 broke compilation on
Solaris, as stdin/stdout/stderr are macros and can't be assigned to.

The original change was to avoid cppcheck warnings. Change to checking
the return value and logging if there was a failure.

lib/src/util/posix/daemonize.cc
@@ -242,9 +242,15 @@ std::unique_ptr<PIDFile> daemonize() {
 
     // Redirect standard files; we always use boost::log anyway
 
-    stdin = freopen("/dev/null", "r", stdin);
-    stdout = freopen("/dev/null", "w", stdout);
-    stderr = freopen("/dev/null", "w", stderr);
+    if (freopen("/dev/null", "r", stdin) == nullptr) {
+        LOG_WARNING("Failed to redirect stdin to /dev/null; errno=%1%", std::to_string(errno));
+    }
+    if (freopen("/dev/null", "w", stdout) == nullptr) {
+        LOG_WARNING("Failed to redirect stdout to /dev/null; errno=%1%", std::to_string(errno));
+    }
+    if (freopen("/dev/null", "w", stderr) == nullptr) {
+        LOG_WARNING("Failed to redirect stderr to /dev/null; errno=%1%", std::to_string(errno));
+    }
 
     LOG_INFO("Daemonization completed; pxp-agent PID=%1%, PID lock file in '%2%'",
              agent_pid, pidfile);