c90d5b1583b35ec5800a22b22b53901c38a3969b
Pieter Loubser <pieter.loubser@puppetlabs.com>
2015-10-26 12:52:20 +0000

    (PCP-119) Add machine readable error_type to response
    Here we add a new field to the response, error_type. This field is
    intended to be machine readable so that consumers of the response
    message can easier determine how to react to failures.
    
    We are also able to refactor #get_result_from_report by adding
    a #make_error_result function. This simplifies it's calling convention
    by removing the need for the optional error string and fail_hard
    boolean. Calls that do not require the last_run_report to be parsed can
    now directly call #make_error_result.

--- modules/spec/unit/modules/puppet_spec.rb
+++ modules/spec/unit/modules/puppet_spec.rb
@@ -12,6 +12,19 @@ describe "pxp-module-puppet" do
     {"puppet_bin" => "puppet"}
   }
 
+  describe "last_run_result" do
+    it "returns the basic structure with exitcode set" do
+      expect(last_run_result(42)).to be == {"kind" => "unknown",
+                                            "time" => "unknown",
+                                            "transaction_uuid" => "unknown",
+                                            "environment" => "unknown",
+                                            "status" => "unknown",
+                                            "error_type" => "",
+                                            "error" => "",
+                                            "exitcode" => 42}
+    end
+  end
+
   describe "check_config_print" do
     it "returns the result of configprint" do
       cli_str = "puppet agent --configprint value"
@@ -71,6 +84,20 @@ describe "pxp-module-puppet" do
     end
   end
 
+  describe "make_error_result" do
+    it "should set the exitcode, error_type and error_message" do
+      expect(make_error_result(42, Errors::FailedToStart, "test error")).to be == 
+          {"kind" => "unknown",
+           "time" => "unknown",
+           "transaction_uuid" => "unknown",
+           "environment" => "unknown",
+           "status" => "unknown",
+           "error_type" => "agent_failed_to_start",
+           "error" => "test error",
+           "exitcode" => 42}
+    end
+  end
+
   describe "get_result_from_report" do
     it "doesn't process the last_run_report if the file doens't exist" do
       allow(File).to receive(:exist?).and_return(false)
@@ -81,6 +108,7 @@ describe "pxp-module-puppet" do
            "transaction_uuid" => "unknown",
            "environment"      => "unknown",
            "status"           => "unknown",
+           "error_type"       => "no_last_run_report",
            "error"            => "/opt/puppetlabs/puppet/cache/state/last_run_report.yaml doesn't exist",
            "exitcode"         => 0}
     end
@@ -95,6 +123,7 @@ describe "pxp-module-puppet" do
            "transaction_uuid" => "unknown",
            "environment"      => "unknown",
            "status"           => "unknown",
+           "error_type"       => "invalid_last_run_report",
            "error"            => "/opt/puppetlabs/puppet/cache/state/last_run_report.yaml could not be loaded: error",
            "exitcode"         => 0}
     end
@@ -114,12 +143,13 @@ describe "pxp-module-puppet" do
       allow_any_instance_of(Object).to receive(:check_config_print).and_return("/opt/puppetlabs/puppet/cache/state/")
       allow(YAML).to receive(:load_file).and_return(last_run_report)
 
-      expect(get_result_from_report(-1, default_config, start_time, "Puppet agent exited with a non 0 exitcode")).to be ==
+      expect(get_result_from_report(-1, default_config, start_time)).to be ==
           {"kind"             => "unknown",
            "time"             => "unknown",
            "transaction_uuid" => "unknown",
            "environment"      => "unknown",
            "status"           => "unknown",
+           "error_type"       => "agent_exit_non_zero",
            "error"            => "Puppet agent exited with a non 0 exitcode",
            "exitcode"         => -1}
     end
@@ -139,12 +169,13 @@ describe "pxp-module-puppet" do
       allow_any_instance_of(Object).to receive(:check_config_print).and_return("/opt/puppetlabs/puppet/cache/state/")
       allow(YAML).to receive(:load_file).and_return(last_run_report)
 
-      expect(get_result_from_report(-1, default_config, start_time, "Puppet agent exited with a non 0 exitcode")).to be ==
+      expect(get_result_from_report(-1, default_config, start_time)).to be ==
           {"kind"             => "apply",
            "time"             => run_time,
            "transaction_uuid" => "ac59acbe-6a0f-49c9-8ece-f781a689fda9",
            "environment"      => "production",
            "status"           => "changed",
+           "error_type"       => "agent_exit_non_zero",
            "error"            => "Puppet agent exited with a non 0 exitcode",
            "exitcode"         => -1}
     end
@@ -170,6 +201,7 @@ describe "pxp-module-puppet" do
            "transaction_uuid" => "ac59acbe-6a0f-49c9-8ece-f781a689fda9",
            "environment"      => "production",
            "status"           => "changed",
+           "error_type"       => "",
            "error"            => "",
            "exitcode"         => 0}
     end
@@ -183,22 +215,20 @@ describe "pxp-module-puppet" do
     it "populates output when it terminated normally" do
       allow(Puppet::Util::Execution).to receive(:execute).and_return(runoutcome)
       allow(runoutcome).to receive(:exitstatus).and_return(0)
-      allow_any_instance_of(Object).to receive(:get_result_from_report).with(0, default_config, anything)
+      expect_any_instance_of(Object).to receive(:get_result_from_report).with(0, default_config, anything)
       start_run(default_config, default_params)
     end
 
     it "populates output when it terminated with a non 0 code" do
       allow(Puppet::Util::Execution).to receive(:execute).and_return(runoutcome)
       allow(runoutcome).to receive(:exitstatus).and_return(1)
-      allow_any_instance_of(Object).to receive(:get_result_from_report).with(1, default_config, anything,
-                                                                        "Puppet agent exited with a non 0 exitcode")
+      expect_any_instance_of(Object).to receive(:get_result_from_report).with(1, default_config, anything)
       start_run(default_config, default_params)
     end
 
     it "populates output when it couldn't start" do
       allow(Puppet::Util::Execution).to receive(:execute).and_return(nil)
-      allow_any_instance_of(Object).to receive(:get_result_from_report).with(-1, default_config, anything,
-                                                                        "Failed to start Puppet agent")
+      expect_any_instance_of(Object).to receive(:make_error_result).with(-1, Errors::FailedToStart, "Failed to start Puppet agent")
       start_run(default_config, default_params)
     end
   end
