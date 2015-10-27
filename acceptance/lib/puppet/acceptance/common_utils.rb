005848e2bdd5faa3fbebfa60941464a4083e65dc
John Duarte <john.duarte@puppetlabs.com>
2015-10-27 08:22:57 -0700

    (PCP-174) Acceptance - expand install utils
    This commit expands the acceptance install_utils in order to
    support installation on the full range of platforms for
    acceptance testing.
    
    It also incorporates the Rakefile structure from puppet/facter/hiera
    in order to allow Jenkins pipeline support for AIO. The Rakefile
    retains usage and checks for SUITE_COMMIT and TEST_TARGET to ensure
    that it continues to work with the existing Jenkins job parameters.

--- acceptance/lib/puppet/acceptance/common_utils.rb
+++ acceptance/lib/puppet/acceptance/common_utils.rb
@@ -1,24 +1,10 @@
-require 'puppet/acceptance/puppet_helpers'
-extend Puppet::Acceptance::PuppetHelpers
-
 module Puppet
   module Acceptance
-    module CAUtils
-
-      def initialize_ssl
-        step 'Agent: Run agent --test first time to gen CSR'
-        # allow exit code of 0 here in case certs are already signed
-        on agents, puppet("agent --test --server #{master} --environment provision"), :acceptable_exit_codes => [1]
-        # TODO: skip next steps if above exited 0
-
-        # Sign all waiting certs
-        step 'Master: sign all certs'
-        on master, puppet('cert --sign --all'), :acceptable_exit_codes => [0,24]
-
-        step 'Agents: Run agent --test second time with signed cert'
-        on agents, puppet("agent --test --server #{master} --environment provision"), :acceptable_exit_codes => [0,2]
+    module CommandUtils
+      def ruby_command(host)
+        "env PATH=\"#{host['privatebindir']}:${PATH}\" ruby"
       end
-
+      module_function :ruby_command
     end
   end
 end
--- acceptance/lib/puppet/acceptance/install_utils.rb
+++ acceptance/lib/puppet/acceptance/install_utils.rb
@@ -8,11 +8,9 @@ module Puppet
     module InstallUtils
       PLATFORM_PATTERNS = {
         :redhat        => /fedora|el|centos/,
-        :debian8       => /debian-?8/,
         :debian        => /debian|ubuntu/,
-        :sles          => /sles/,
-        :solaris_10    => /solaris-10/,
-        :solaris_11    => /solaris-11/,
+        :debian_ruby18 => /debian|ubuntu-lucid|ubuntu-precise/,
+        :solaris       => /solaris/,
         :windows       => /windows/,
       }.freeze
 
@@ -56,15 +54,59 @@ module Puppet
         return true
       end
 
+      def fetch(base_url, file_name, dst_dir)
+        FileUtils.makedirs(dst_dir)
+        src = "#{base_url}/#{file_name}"
+        dst = File.join(dst_dir, file_name)
+        if File.exists?(dst)
+          logger.notify "Already fetched #{dst}"
+        else
+          logger.notify "Fetching: #{src}"
+          logger.notify "  and saving to #{dst}"
+          open(src) do |remote|
+            File.open(dst, "w") do |file|
+              FileUtils.copy_stream(remote, file)
+            end
+          end
+        end
+        return dst
+      end
+
+      def fetch_remote_dir(url, dst_dir)
+        logger.notify "fetch_remote_dir (url: #{url}, dst_dir #{dst_dir})"
+        if url[-1, 1] !~ /\//
+          url += '/'
+        end
+        url = URI.parse(url)
+        chunks = url.path.split('/')
+        dst = File.join(dst_dir, chunks.last)
+        #determine directory structure to cut
+        #only want to keep the last directory, thus cut total number of dirs - 2 (hostname + last dir name)
+        cut = chunks.length - 2
+        wget_command = "wget -nv -P #{dst_dir} --reject \"index.html*\",\"*.gif\" --cut-dirs=#{cut} -np -nH --no-check-certificate -r #{url}"
+
+        logger.notify "Fetching remote directory: #{url}"
+        logger.notify "  and saving to #{dst}"
+        logger.notify "  using command: #{wget_command}"
+
+        #in ruby 1.9+ we can upgrade this to popen3 to gain access to the subprocess pid
+        result = `#{wget_command} 2>&1`
+        result.each_line do |line|
+          logger.debug(line)
+        end
+        if $?.to_i != 0
+          raise "Failed to fetch_remote_dir '#{url}' (exit code #{$?}"
+        end
+        dst
+      end
+
       def stop_firewall_on(host)
         case host['platform']
         when /debian/
           on host, 'iptables -F'
         when /fedora|el-7/
           on host, puppet('resource', 'service', 'firewalld', 'ensure=stopped')
-          on host, puppet('resource', 'service', 'iptables', 'ensure=stopped')
         when /el|centos/
-          on host, puppet('resource', 'service', 'firewalld', 'ensure=stopped')
           on host, puppet('resource', 'service', 'iptables', 'ensure=stopped')
         when /ubuntu/
           on host, puppet('resource', 'service', 'ufw', 'ensure=stopped')
@@ -75,72 +117,55 @@ module Puppet
 
       def install_repos_on(host, project, sha, repo_configs_dir)
         platform = host['platform'].with_version_codename
+        platform_configs_dir = File.join(repo_configs_dir,platform)
         tld     = sha == 'nightly' ? 'nightlies.puppetlabs.com' : 'builds.puppetlabs.lan'
-        project = sha == 'nightly' ? project + '-latest'        :  project
-        sha     = sha == 'nightly' ? nil                        :  sha
+        project = sha == 'nightly' ? project + '-latest'        : project
+        sha     = sha == 'nightly' ? nil                        : sha
 
         case platform
-        when /^(fedora|el|centos)-(\d+)-(.+)$/
-          variant = (($1 == 'centos') ? 'el' : $1)
-          fedora_prefix = ((variant == 'fedora') ? 'f' : '')
-          version = $2
-          arch = $3
+          when /^(fedora|el|centos)-(\d+)-(.+)$/
+            variant = (($1 == 'centos') ? 'el' : $1)
+            fedora_prefix = ((variant == 'fedora') ? 'f' : '')
+            version = $2
+            arch = $3
 
-          # install repo so deps can be fulfilled when required
-          # todo: this should be moved outside of here so we don't reinstall with each repos installed
-          puppetlabs_repo_url = "http://yum.puppetlabs.com/puppetlabs-release-%s-%s.noarch.rpm" % [variant, version]
-          on host, "rpm -Uvh --force #{puppetlabs_repo_url}"
+            repo_filename = "pl-%s%s-%s-%s%s-%s.repo" % [
+              project,
+              sha ? '-' + sha : '',
+              variant,
+              fedora_prefix,
+              version,
+              arch
+            ]
+            repo_url = "http://%s/%s/%s/repo_configs/rpm/%s" % [tld, project, sha, repo_filename]
 
-          # grab the repo for SHA of project
-          repo_filename = "pl-%s%s%s-%s-%s%s-%s.repo" % [
-            project,
-            sha ? '-' + sha : '',
-            project =~ /xnode-service/ ? '-repos-pe' : '',
-            variant,
-            fedora_prefix,
-            version,
-            arch
-          ]
-          repo_url = "http://%s/%s/%s/repo_configs/rpm/%s" % [tld, project, sha, repo_filename]
+            on host, "curl -o /etc/yum.repos.d/#{repo_filename} #{repo_url}"
+          when /^(debian|ubuntu)-([^-]+)-(.+)$/
+            variant = $1
+            version = $2
+            arch = $3
 
-          on host, "curl -o /etc/yum.repos.d/#{repo_filename} #{repo_url}"
-        when /^(debian|ubuntu)-([^-]+)-(.+)$/
-          variant = $1
-          version = $2
-          arch = $3
+            list_filename = "pl-%s%s-%s.list" % [
+              project,
+              sha ? '-' + sha : '',
+              version
+            ]
+            list_url = "http://%s/%s/%s/repo_configs/deb/%s" % [tld, project, sha, list_filename]
 
-          # install repo so deps can be fulfilled when required
-          # todo: this should be moved outside of here so we don't reinstall with each repos installed
-          deb_filename = "puppetlabs-release-#{version}.deb"
-          puppetlabs_repo_url = "http://apt.puppetlabs.com/#{deb_filename}"
-          puppetlabs_repo_path = "/tmp/#{deb_filename}"
-          on host, "curl -o #{puppetlabs_repo_path} #{puppetlabs_repo_url}"
-          on host, "dpkg -i --force-all #{puppetlabs_repo_path}"
-
-          list_filename = "pl-%s%s-%s.list" % [
-            project,
-            sha ? '-' + sha : '',
-            version
-          ]
-          list_url = "http://%s/%s/%s/repo_configs/deb/%s" % [tld, project, sha, list_filename]
-
-          on host, "curl -o /etc/apt/sources.list.d/#{list_filename} #{list_url}"
-          on host, 'apt-get update'
-        else
-          host.logger.notify("No repository installation step for #{platform} yet...")
-        end
-      end
-
-      # Configures gem sources on hosts to use a mirror, if specified
-      # This is a duplicate of the Gemfile logic.
-      def configure_gem_mirror(hosts)
-        hosts = [hosts] unless hosts.kind_of?(Array)
-        gem_source = ENV['GEM_SOURCE'] || 'https://rubygems.org'
-
-        hosts.each do |host|
-          gem = Puppet::Acceptance::CommandUtils.gem_command(host)
-          on host, "#{gem} source --clear-all"
-          on host, "#{gem} source --add #{gem_source}"
+            on host, "curl -o /etc/apt/sources.list.d/#{list_filename} #{list_url}"
+            on host, "apt-get update"
+          else
+            if project == 'puppet-agent'
+                opts = {
+                    :puppet_collection => 'PC1',
+                    :puppet_agent_sha => ENV['SHA'],
+                    :puppet_agent_version => ENV['SUITE_VERSION'] || ENV['SHA']
+                }
+                # this installs puppet-agent on windows (msi) and osx (dmg)
+                install_puppet_agent_dev_repo_on(agent, opts)
+            else
+                fail_test("No repository installation step for #{platform} yet...")
+            end
         end
       end
     end
