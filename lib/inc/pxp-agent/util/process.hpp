a5ef3d9debc9b0df36e51996669b55ca6d611389
parisiale <alessandro@puppetlabs.com>
2015-11-03 22:35:05 +0000

    (PCP-129) Check if process exists by PID on Windows
    Adding new function for Windows based on OpenProcess(). Note that it
    only checks if a process exists, it doesn't ensure that it's running.
    
    Refactoring POSIX counterpart to new util/process files.
    Adding unit tests.

---
+++ lib/inc/pxp-agent/util/process.hpp
@@ -0,0 +1,12 @@
+#ifndef SRC_UTIL_PROCESS_HPP_
+#define SRC_UTIL_PROCESS_HPP_
+
+namespace PXPAgent {
+namespace Util {
+
+bool processExists(int pid);
+
+}  // namespace Util
+}  // namespace PXPAgent
+
+#endif  // SRC_UTIL_PROCESS_HPP_
