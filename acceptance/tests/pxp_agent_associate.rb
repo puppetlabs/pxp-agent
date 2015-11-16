0f3d90eaa95b98fb303ac6909d202d7117814686
james-stocks <james.stocks@puppetlabs.com>
2015-11-16 17:17:52 +0000

    (maint) Acceptance - use helper method to access log file in association test

--- acceptance/tests/pxp_agent_associate.rb
+++ acceptance/tests/pxp_agent_associate.rb
@@ -2,16 +2,11 @@ test_name 'C93807 - Associate pxp-agent with a PCP broker'
 
 agent1 = agents[0]
 
-log_file = ''
-agent1.platform.start_with?('windows') ?
-  log_file = '/cygdrive/c/ProgramData/PuppetLabs/pxp-agent/var/log/pxp-agent.log' :
-  log_file = '/var/log/puppetlabs/pxp-agent/pxp-agent.log'
-
 step 'Stop pxp-agent if it is currently running'
 on agent1, puppet('resource service pxp-agent ensure=stopped')
 
 step 'Clear existing logs so we don\'t match an existing association entry'
-on(agent1, "rm -rf #{log_file}")
+on(agent1, "rm -rf #{logfile(agent1)}")
 
 step 'Start pxp-agent service'
 on agent1, puppet('resource service pxp-agent ensure=running')
@@ -21,7 +16,7 @@ sleep(10)
 
 websocket_success = /INFO.*Successfully established a WebSocket connection with the PCP broker/
 association_success = /INFO.*Received associate session response.*success/
-on(agent1, "cat #{log_file}") do |result|
+on(agent1, "cat #{logfile(agent1)}") do |result|
   log_contents = result.stdout
   step 'Check pxp-agent.log for websocket connection'
   assert_match(websocket_success, log_contents,
