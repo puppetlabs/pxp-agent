fe364b3fe60b3b38d8778363649ec519d782c366
james-stocks <james.stocks@puppetlabs.com>
2015-11-16 14:52:07 +0000

    (maint) Acceptance - Create a helper for pxp-agent config
    Move config strings (e.g. SSL certs, broker URI) into a helper file, so that test and pre-suite code is neater and easier to maintain.

---
+++ acceptance/lib/pxp-agent/config_helper.rb
@@ -0,0 +1,151 @@
+# This file contains common strings and functions for pxp-agent acceptance tests to use
+
+PXP_CONFIG_DIR_CYGPATH = '/cygdrive/c/ProgramData/PuppetLabs/pxp-agent/etc/'
+PXP_CONFIG_DIR_POSIX = '/etc/puppetlabs/pxp-agent/'
+
+PCP_BROKER_PORT = 8142
+
+SSL_BASE_DIR_WINDOWS = "C:\\\\cygwin64\\\\home\\\\Administrator\\\\test-resources\\\\ssl\\\\"
+SSL_BASE_DIR_POSIX = "/root/test-resources/ssl/"
+ALT_SSL_BASE_DIR_WINDOWS = "C:\\\\cygwin64\\\\home\\\\Administrator\\\\test-resources\\\\ssl\\\\alternative\\\\"
+ALT_SSL_BASE_DIR_POSIX = "/root/test-resources/ssl/alternative/"
+SSL_KEY_FILE_DIR_WINDOWS = "private_keys\\\\"
+SSL_KEY_FILE_DIR_POSIX = "private_keys/"
+SSL_CA_FILE_DIR_WINDOWS = "ca\\\\"
+SSL_CA_FILE_DIR_POSIX = "ca/"
+SSL_CERT_FILE_DIR_WINDOWS = "certs\\\\"
+SSL_CERT_FILE_DIR_POSIX = "certs/"
+
+def left_pad_with_zero(number)
+  if (number.to_s.length == 1)
+    "0#{number}"
+  else
+    number
+  end
+end
+
+def windows?(host)
+  host.platform.upcase.start_with?('WINDOWS')
+end
+
+# @param broker_host the host name or beaker host object for the pcp-broker host
+# @return the broker-ws-uri config string
+def broker_ws_uri(broker_host)
+  "wss://#{master}:#{PCP_BROKER_PORT}/pcp/"
+end
+
+# @param host the beaker host (to determine the correct path for the OS)
+# @return path to the pxp-agent.conf file for that host (a cygpath in the case of Windows)
+def pxp_agent_config_file(host)
+  windows?(host)?
+    "#{PXP_CONFIG_DIR_CYGPATH}pxp-agent.conf" :
+    "#{PXP_CONFIG_DIR_POSIX}pxp-agent.conf"
+end
+
+def ssl_key_file_windows(client_number)
+  client_number = left_pad_with_zero(client_number)
+  "#{SSL_BASE_DIR_WINDOWS}#{SSL_KEY_FILE_DIR_WINDOWS}client#{client_number}.example.com.pem"
+end
+
+def ssl_key_file_posix(client_number)
+  client_number = left_pad_with_zero(client_number)
+  "#{SSL_BASE_DIR_POSIX}#{SSL_KEY_FILE_DIR_POSIX}client#{client_number}.example.com.pem"
+end
+
+# @param host a beaker host (to determine the correct path for the OS)
+# @param client_number which client 01-05 you want the key file for
+# @return the path to the client ssl key file on this host
+def ssl_key_file(host, client_number)
+  windows?(host)?
+    ssl_key_file_windows(client_number) :
+    ssl_key_file_posix(client_number)
+end
+
+def alt_ssl_key_file_windows(client_number)
+  client_number = left_pad_with_zero(client_number)
+  "#{ALT_SSL_BASE_DIR_WINDOWS}#{SSL_KEY_FILE_DIR_WINDOWS}client#{client_number}.alt.example.com.pem"
+end
+
+def alt_ssl_key_file_posix(client_number)
+  client_number = left_pad_with_zero(client_number)
+  "#{ALT_SSL_BASE_DIR_POSIX}#{SSL_KEY_FILE_DIR_POSIX}client#{client_number}.alt.example.com.pem"
+end
+
+# @param host the beaker host (to determine the correct path for the OS)
+# @param client_number which client 01-05 you want the key file for
+# @return the path to the alternative client ssl key file on this host
+def alt_ssl_key_file(host, client_number)
+  windows?(host)?
+    alt_ssl_key_file_windows(client_number) :
+    alt_ssl_key_file_posix(client_number)
+end
+
+def ssl_ca_file_windows
+  "#{SSL_BASE_DIR_WINDOWS}#{SSL_CA_FILE_DIR_WINDOWS}ca_crt.pem"
+end
+
+def ssl_ca_file_posix
+  "#{SSL_BASE_DIR_POSIX}#{SSL_CA_FILE_DIR_POSIX}ca_crt.pem"
+end
+
+# @param host the beaker host (to determine the correct path for the OS)
+# @return the path to the ssl ca file on this host
+def ssl_ca_file(host)
+  windows?(host)?
+    ssl_ca_file_windows :
+    ssl_ca_file_posix
+end
+
+def alt_ssl_ca_file_windows
+  "#{ALT_SSL_BASE_DIR_WINDOWS}#{SSL_CA_FILE_DIR_WINDOWS}ca_crt.pem"
+end
+
+def alt_ssl_ca_file_posix
+  "#{ALT_SSL_BASE_DIR_POSIX}#{SSL_CA_FILE_DIR_POSIX}ca_crt.pem"
+end
+
+# @param host the beaker host (to determine the correct path for the OS)
+# @return the path to the alternative ssl ca file on this host
+def alt_ssl_ca_file(host)
+  windows?(host)?
+    alt_ssl_ca_file_windows :
+    alt_ssl_ca_file_posix
+end
+
+def ssl_cert_file_windows(client_number)
+  client_number = left_pad_with_zero(client_number)
+  "#{SSL_BASE_DIR_WINDOWS}#{SSL_CERT_FILE_DIR_WINDOWS}client#{client_number}.example.com.pem"
+end
+
+def ssl_cert_file_posix(client_number)
+  client_number = left_pad_with_zero(client_number)
+  "#{SSL_BASE_DIR_POSIX}#{SSL_CERT_FILE_DIR_POSIX}client#{client_number}.example.com.pem"
+end
+
+# @param host the beaker host (to determine the correct path for the OS)
+# @param client_number which client 01-05 you want the key file for
+# @return the path to the ssl cert for this client on this host
+def ssl_cert_file(host, client_number)
+  windows?(host)?
+    ssl_cert_file_windows(client_number) :
+    ssl_cert_file_posix(client_number)
+end
+
+def alt_ssl_cert_file_windows(client_number)
+  client_number = left_pad_with_zero(client_number)
+  "#{ALT_SSL_BASE_DIR_WINDOWS}#{SSL_CERT_FILE_DIR_WINDOWS}client#{client_number}.alt.example.com.pem"
+end
+
+def alt_ssl_cert_file_posix(client_number)
+  client_number = left_pad_with_zero(client_number)
+  "#{ALT_SSL_BASE_DIR_POSIX}#{SSL_CERT_FILE_DIR_POSIX}client#{client_number}.alt.example.com.pem"
+end
+
+# @param host the beaker host (to determine the correct path for the OS)
+# @param client_number which client 01-05 you want the key file for
+# @return the path to the alternative ssl cert for this client on this host
+def alt_ssl_cert_file(host, client_number)
+  windows?(host)?
+    alt_ssl_cert_file_windows(client_number) :
+    alt_ssl_cert_file_posix(client_number)
+end
--- acceptance/setup/common/020_Configure_Pxp_Agents.rb
+++ acceptance/setup/common/020_Configure_Pxp_Agents.rb
@@ -1,29 +1,19 @@
+require 'pxp-agent/config_helper.rb'
+
 step 'Copy test certs to agents'
 agents.each do |agent|
   scp_to(agent, '../test-resources', 'test-resources')
 end
 
 step 'Create config file'
-agents.each do |agent|
-  if (agent.platform.upcase.start_with?('WINDOWS'))
-    config = <<OUTPUT
+agents.each_with_index do |agent, i|
+  config = <<OUTPUT
 {
-  "broker-ws-uri" : "wss://#{master}:8142/pcp/",
-  "ssl-key" : "C:\\\\cygwin64\\\\home\\\\Administrator\\\\test-resources\\\\ssl\\\\private_keys\\\\client01.example.com.pem",
-  "ssl-ca-cert" : "C:\\\\cygwin64\\\\home\\\\Administrator\\\\test-resources\\\\ssl\\\\ca\\\\ca_crt.pem",
-  "ssl-cert" : "C:\\\\cygwin64\\\\home\\\\Administrator\\\\test-resources\\\\ssl\\\\certs\\\\client01.example.com.pem"
+  "broker-ws-uri" : "#{broker_ws_uri(master)}",
+  "ssl-key" : "#{ssl_key_file(agent, i+1)}",
+  "ssl-ca-cert" : "#{ssl_ca_file(agent)}",
+  "ssl-cert" : "#{ssl_cert_file(agent, i+1)}"
 }
 OUTPUT
-    create_remote_file(agent, '/cygdrive/c/ProgramData/PuppetLabs/pxp-agent/etc/pxp-agent.conf', config)
-  else
-    config = <<OUTPUT
-{
-  "broker-ws-uri" : "wss://#{master}:8142/pcp/",
-  "ssl-key" : "/root/test-resources/ssl/private_keys/client01.example.com.pem",
-  "ssl-ca-cert" : "/root/test-resources/ssl/ca/ca_crt.pem",
-  "ssl-cert" : "/root/test-resources/ssl/certs/client01.example.com.pem"
-}
-OUTPUT
-    create_remote_file(agent, '/etc/puppetlabs/pxp-agent/pxp-agent.conf', config)
-  end
+  create_remote_file(agent, pxp_agent_config_file(agent), config)
 end
