7e86f82655508621f87b05d556771ce369b358c7
james-stocks <james.stocks@puppetlabs.com>
2016-01-04 15:56:35 +0000

    (PCP-223) Update acceptance setup on stable branch
    Keep acceptance on stable up to date with master.

--- acceptance/config/aio/options.rb
+++ acceptance/config/aio/options.rb
@@ -1,18 +1,10 @@
 {
-  :log_level     => 'info',
-  :type          => 'packages',
-  :forge_host    => 'forge-aio01-petest.puppetlabs.com',
-  :load_path     => './lib/',
-  :repo_proxy    => true,
-  :add_el_extras => true,
-  :'puppetserver-confdir' => '/etc/puppetserver/conf.d',
-  :pre_suite     => [
-    'setup/aio/010_Install.rb',
+  :type => 'aio',
+  :pre_suite => [
+    'setup/common/000-delete-puppet-when-none.rb',
+    'setup/aio/pre-suite/010_Install.rb',
     'setup/aio/021_InstallAristaModule.rb',
     'setup/common/010_Setup_Broker.rb',
-    'setup/common/020_Configure_Pxp_Agents.rb',],
-  :ssh           => {
-    :keys => ["id_rsa_acceptance", "#{ENV['HOME']}/.ssh/id_rsa-acceptance",
-              "id_rsa", "#{ENV['HOME']}/.ssh/id_rsa"],
-  },
+    'setup/common/020_Configure_Pxp_Agents.rb',
+  ],
 }
