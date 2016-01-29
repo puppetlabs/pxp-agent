05f6acfe0f2af2f7300c1fd33af1a30d5f45b036
parisiale <alessandro@puppetlabs.com>
2016-01-29 14:12:43 +0000

    (PCP-207) Update external modules used for testing
    Here we ensure that the modules used by external_module_test.cc do
    provide output as requested by the new interface.

---
+++ lib/tests/resources/modules/check_output.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+def check_output_files(args)
+  output_files = args["output_files"]
+  if !output_files
+    return
+  end
+
+  $stdout.reopen(File.open(output_files["stdout"], 'w'))
+  $stderr.reopen(File.open(output_files["stderr"], 'w'))
+
+  at_exit do
+    status = if $!.nil?
+      0
+    elsif $!.is_a?(SystemExit)
+      $!.status
+    else
+      1
+    end
+    File.open(output_files["exitcode"], 'w') do |f|
+      f.puts(status)
+    end
+  end
+end
