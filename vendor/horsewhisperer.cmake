f092361dec9c3f8e7647dd60db840a9744f6f5bf
parisiale <alessandro@puppetlabs.com>
2016-06-01 15:40:03 +0100

    (maint) Update to HorseWhisperer 0.12.1
    Updating to the latest tag so that we can benefit from the hidden
    options definition.

--- vendor/horsewhisperer.cmake
+++ vendor/horsewhisperer.cmake
@@ -4,8 +4,8 @@ include(ExternalProject)
 externalproject_add(
     horsewhisperer
     PREFIX "${PROJECT_BINARY_DIR}"
-    URL "file://${VENDOR_DIRECTORY}/horsewhisperer-0.12.0.zip"
-    URL_MD5 "45a69e06ce7d480725578de7be788ff1"
+    URL "file://${VENDOR_DIRECTORY}/horsewhisperer-0.12.1.zip"
+    URL_MD5 "eaf10192cede3c68e46e36ffdb19297f"
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     BUILD_IN_SOURCE 1
