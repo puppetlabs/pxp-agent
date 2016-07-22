cc8512e8bf00b7f2d4082e7462f00a4585f53ccc
james-stocks <james.stocks@puppetlabs.com>
2016-07-22 16:42:36 +0000

    (PCP-514) Acceptance - change --version command for Cisco Nexus
    Cisco Nexus does not accept commands with an inline env value at the start e.g. 'LIB_PATH=bar /usr/bin/foo'
    For the version test, we needed to give the path to vendored Ruby so that pxp-agent would work from a cygwin shell.
    Since only Windows requires this, this commit sets PATH only for Windows agents.
    
    [skip ci]

--- acceptance/tests/pxp_agent_version.rb
+++ acceptance/tests/pxp_agent_version.rb
@@ -4,8 +4,13 @@ test_name 'C93805 - pxp-agent - Versioning test'
 
 agents.each do |agent|
   step 'cd into pxp-agent bin folder and check the version' do
-    # Vendored Ruby needs to be on path. The "privatebindir" property of beaker host can be used for this
-    on(agent, "PATH=\"#{agent['privatebindir']}:$PATH\" #{pxp_agent_dir(agent)}/pxp-agent --version") do |result|
+    version_command = "#{pxp_agent_dir(agent)}/pxp-agent --version"
+    # Vendored Ruby needs to be on path for Windows/cygwin.
+    # Only do this for cygwin because other OSes (Cisco Nexus) do not handle setting env values inline.
+    if windows?(agent) then
+      version_command = "PATH=\"#{agent['privatebindir']}:$PATH\"" + " " + version_command
+    end
+    on(agent, version_command) do |result|
       assert(/[0-9\.]*/ =~ result.stdout, "Version number should be numbers and periods but was \"#{result.stdout.to_s}\"")
     end
   end
