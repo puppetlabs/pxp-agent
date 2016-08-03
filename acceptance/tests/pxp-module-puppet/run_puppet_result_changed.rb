5d1ee5d7d9c8696f69198db4b2c949d47709ef34
james-stocks <james.stocks@puppetlabs.com>
2016-08-03 17:02:43 +0000

    (maint) Acceptance - make run puppet tests use environment helper
    [skip ci]

--- acceptance/tests/pxp-module-puppet/run_puppet_result_changed.rb
+++ acceptance/tests/pxp-module-puppet/run_puppet_result_changed.rb
@@ -1,13 +1,14 @@
 require 'pxp-agent/test_helper.rb'
-
-ENVIRONMENT_NAME = 'changed_result'
+require 'puppet/acceptance/environment_utils'
 
 test_name 'C93062 - Run puppet and expect \'changed\' result' do
+  extend Puppet::Acceptance::EnvironmentUtils
+
+  env_name = test_file_name = File.basename(__FILE__, '.*')
+  environment_name = mk_tmp_environment(env_name)
 
   step 'On master, create a new environment that will result in Changed' do
-    environmentpath = master.puppet['environmentpath']
-    site_manifest = "#{environmentpath}/#{ENVIRONMENT_NAME}/manifests/site.pp"
-    on(master, "cp -r #{environmentpath}/production #{environmentpath}/#{ENVIRONMENT_NAME}")
+    site_manifest = "#{environmentpath}/#{environment_name}/manifests/site.pp"
     create_remote_file(master, site_manifest, <<-SITEPP)
 node default {
   notify {'Notify resources cause a Puppet run to have a \\'changed\\' outcome ☃':}
@@ -37,7 +38,7 @@ SITEPP
     begin
       responses = rpc_blocking_request(master, target_identities,
                                       'pxp-module-puppet', 'run',
-                                      {:env => [], :flags => ['--environment', ENVIRONMENT_NAME]})
+                                      {:env => [], :flags => ['--environment', environment_name]})
     rescue => exception
       fail("Exception occurred when trying to run Puppet on all agents: #{exception.message}")
     end
@@ -48,8 +49,8 @@ SITEPP
         # The test's pass/fail criteria is only the value of 'status'. However, if something goes wrong and Puppet needs to default
         # the environment to 'production' and results in 'unchanged' then it's better to fail specifically on the environment.
         assert(action_result.has_key?('environment'), "Results for pxp-module-puppet run on #{agent} should contain an 'environment' field")
-        assert_equal(ENVIRONMENT_NAME, action_result['environment'], "Result of pxp-module-puppet run on #{agent} should run with the "\
-                                                                     "#{ENVIRONMENT_NAME} environment")
+        assert_equal(environment_name, action_result['environment'], "Result of pxp-module-puppet run on #{agent} should run with the "\
+                                                                     "#{environment_name} environment")
         assert(action_result.has_key?('status'), "Results for pxp-module-puppet run on #{agent} should contain a 'status' field")
         assert_equal('changed', action_result['status'], "Result of pxp-module-puppet run on #{agent} should be 'changed'")
       end
