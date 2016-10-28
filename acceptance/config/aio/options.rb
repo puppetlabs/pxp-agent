5c4e34b30d3337ae5e7608981423a0a8c11eb20b
james-stocks <james.stocks@puppetlabs.com>
2016-10-28 13:38:28 +0000

    (maint) Remove unnecessary acceptance/setup/aio/pre-suite folder
    Don't need a pre-suite subfolder and it makes the hierarchy confusing, get rid of it.
    
    [skip ci]

--- acceptance/config/aio/options.rb
+++ acceptance/config/aio/options.rb
@@ -8,7 +8,7 @@
   :pre_suite => [
     'setup/common/000-delete-puppet-when-none.rb',
     'setup/common/005_SyncTime.rb',
-    'setup/aio/pre-suite/010_Install.rb',
+    'setup/aio/010_Install.rb',
     'setup/aio/021_InstallAristaModule.rb',
     'setup/common/035_StartPuppetServer.rb',
     'setup/common/040_ValidateSignCert.rb',
