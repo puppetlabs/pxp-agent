88f7b2ff117d68c259bda91e074ee0b7d3140474
Glenn Sarti <glennsarti@users.noreply.github.com>
2016-12-05 11:07:08 -0800

    (PCP-625) Skip beaker timesync on Windows 10
    Previously we skipped timesync on Windows Server 2016 in commit cd334727317ac4f81294ff1e9709accc728f0fb3
    However Windows 10 shares the same kernel and requires a similar fix.  This
    commit adds a Windows 10 exclusion for the beaker timesync process.
    
    [skip ci]

--- acceptance/setup/common/005_SyncTime.rb
+++ acceptance/setup/common/005_SyncTime.rb
@@ -8,9 +8,10 @@ test_name 'Ensure hosts have synchronized clocks'
 # Affects OSX and Ubuntu on vmpooler
 applicable_hosts = hosts.select{|host| host['platform'] !~ /osx|ubuntu/}
 
-# PCP-625 the beaker timesync method causes strange behavior on Windows Server 2016,
+# PCP-625 the beaker timesync method causes strange behavior on Windows Server 2016 and Windows 10,
 # temporarily setting the date far into the past, which breaks SSL certificate retrieval
 applicable_hosts = applicable_hosts.select{|host| host['platform'] !~ /windows-2016/}
+applicable_hosts = applicable_hosts.select{|host| host['platform'] !~ /windows-10/}
 
 # BKR-960 - timesync does not work on Cisco XR
 applicable_hosts = applicable_hosts.select{|host| host['platform'] !~ /cisco_ios_xr/} 
