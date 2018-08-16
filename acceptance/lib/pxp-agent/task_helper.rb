7b65a1e634eb4c4ff109fe13125ae67ded444b98
Michael Smith <michael.smith@puppet.com>
2018-08-15 17:10:44 -0700

    (PCP-835) Add test with implementations
    Adds a test that uses the 'puppet-agent' feature to resolve the correct
    file to run. Updates the run_task helpers to accept task metadata.

--- acceptance/lib/pxp-agent/task_helper.rb
+++ acceptance/lib/pxp-agent/task_helper.rb
@@ -5,14 +5,12 @@ def file_entry(filename, sha256, path = 'foo')
   {:uri => {:path => path, :params => {}}, :filename => filename, :sha256 => sha256}
 end
 
-def run_task(broker, target_identities, task, input, files, rpc_request, expected_response_type, &check_datas)
+def run_task(broker, target_identities, task, input, files, metadata, rpc_request, expected_response_type, &check_datas)
   responses =
     begin
-      rpc_request.call(broker, target_identities,
-                               'task', 'run',
-                               {:task => task,
-                                :input => input,
-                                :files => files})
+      params = { task: task, input: input, files: files }
+      params = params.merge(metadata: metadata) if metadata
+      rpc_request.call(broker, target_identities, 'task', 'run', params)
     rescue => exception
       fail("Exception occurred when trying to run task '#{task}' on all agents: #{exception.message}")
     end
@@ -30,10 +28,11 @@ end
 
 # Runs a task on targets, and passes the output to the block for validation
 # Block should expect a map parsed from the JSON output
-def run_successful_task(broker, targets, task, files, input: {}, max_retries: 30, query_interval: 1, &block)
+def run_successful_task(broker, targets, task, files, input: {}, metadata: nil, max_retries: 30, query_interval: 1, &block)
   target_identities = targets.map {|agent| "pcp://#{agent}/agent"}
 
-  run_task(broker, target_identities, task, input, files, method(:rpc_non_blocking_request), "http://puppetlabs.com/rpc_provisional_response") do |datas|
+  run_task(broker, target_identities, task, input, files, metadata,
+           method(:rpc_non_blocking_request), "http://puppetlabs.com/rpc_provisional_response") do |datas|
     transaction_ids = datas.map { |data| data["transaction_id"] }
     target_identities.zip(transaction_ids).map do |identity, transaction_id|
       check_non_blocking_response(broker, identity, transaction_id, max_retries, query_interval) do |result|
@@ -46,10 +45,11 @@ def run_successful_task(broker, targets, task, files, input: {}, max_retries: 30
   end
 end
 
-def run_failed_task(broker, targets, task, files, input: {}, max_retries: 30, query_interval: 1, &block)
+def run_failed_task(broker, targets, task, files, input: {}, metadata: nil, max_retries: 30, query_interval: 1, &block)
   target_identities = targets.map {|agent| "pcp://#{agent}/agent"}
 
-  run_task(broker, target_identities, task, input, files, method(:rpc_non_blocking_request), "http://puppetlabs.com/rpc_provisional_response") do |datas|
+  run_task(broker, target_identities, task, input, files, metadata,
+           method(:rpc_non_blocking_request), "http://puppetlabs.com/rpc_provisional_response") do |datas|
     transaction_ids = datas.map { |data| data["transaction_id"] }
     target_identities.zip(transaction_ids).map do |identity, transaction_id|
       check_non_blocking_response(broker, identity, transaction_id, max_retries, query_interval) do |result|
@@ -64,10 +64,11 @@ end
 
 # Runs a task that's expected to result in a PXP error. Block should
 # expect the description of the PXP error to verify.
-def run_pxp_errored_task(broker, targets, task, files, input: {}, &block)
+def run_pxp_errored_task(broker, targets, task, files, input: {}, metadata: nil, &block)
   target_identities = targets.map {|agent| "pcp://#{agent}/agent"}
 
-  run_task(broker, target_identities, task, input, files, method(:rpc_blocking_request), "http://puppetlabs.com/rpc_error_message") do |datas|
+  run_task(broker, target_identities, task, input, files, metadata,
+           method(:rpc_blocking_request), "http://puppetlabs.com/rpc_error_message") do |datas|
     descriptions = datas.map { |data| data["description"] }
     descriptions.each { |description| block.call(description) }
   end
